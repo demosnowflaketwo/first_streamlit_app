bf7c201d87878c04031f1ac3224f194b5b8d4468
anur <110472140+demosnowflaketwo@users.noreply.github.com>
2022-08-02 16:52:14 +0300

    Update streamlit_app.py
    wrote the breakfast menu
    and emojis and smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.title('My dinner')
+st.title('🥣 My healthy Diner')
+st.header('🥗Breakfast menu')
+st.text('🐔pancakes & oatmeal')
+st.text('🥑poriage')
+st.text(' 🍞banna waffle')
+
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
