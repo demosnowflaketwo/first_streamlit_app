c2501296b788d8e37ad3a3842237edb53fbe7212
anur <110472140+demosnowflaketwo@users.noreply.github.com>
2022-08-02 16:43:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,2 @@
 import streamlit as st
-APP_NAME = "HEalthy Dinner"
-st.set_page_config(
-    page_title=APP_NAME,
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+st.title('My dinner')
