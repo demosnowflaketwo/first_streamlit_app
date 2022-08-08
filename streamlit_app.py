5ed06089cdf0b1fbe08d96a2814c188a3eabc64d
anur <110472140+demosnowflaketwo@users.noreply.github.com>
2022-08-08 17:45:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.header("Fruityvice Fruit Advice!")
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # st.text(fruityvice_response.json()) # this just writes the data on the screen
 
